8c0809a4ab1712e3ce1b9d81c387397f3fc66a84
Samvel Vardanyan <developer.vardanyan@gmail.com>
2021-09-14 20:03:33 +0400

    add files

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dolores natus cupiditate deleniti dolorem ad, tenetur repellendus quo cum molestias animi. Ut laudantium error tempora quo, consequuntur animi optio consequatur?</p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
---
+++ src/views/Todos.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h2>Todo application</h2>
+    <router-link to="/">Home</router-link>
+    <hr>
+    <AddTodo
+      @add-todo="addTodo"
+    />
+    <hr>
+    <Loader
+      v-if="loading"
+    />
+    <TodoList
+      v-else-if="todos.length"
+      v-bind:todos="todos"
+      @remove-todo="removeTodo"
+    />
+    <p v-else>No todos!</p>
+  </div>
+</template>
+
+<script>
+  import TodoList from "@/components/TodoList"
+  import Loader  from "@/components/Loader"
+  import AddTodo  from "@/components/AddTodo"
+  export default {
+    name: 'App',
+    data() {
+      return {
+        todos: [],
+        loading: true
+      }
+    },
+    mounted() {
+      fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+        .then(response => response.json())
+        .then(json => {
+          setTimeout(() => {
+            this.todos = json
+            this.loading = false
+          }, 1000);
+        });
+    },
+    methods: {
+      removeTodo(id) {
+        this.todos = this.todos.filter(t => t.id !== id)
+      },
+      addTodo(todo) {
+        this.todos.push(todo)
+      }
+    },
+    components: {
+      TodoList, AddTodo, Loader
+    }
+  }
+</script>
